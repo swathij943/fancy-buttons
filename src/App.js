b815a5e09b315e996cbcd1fcf723d50195a5f21c
swathij943 <swathij943@gmail.com>
2023-08-15 19:27:06 -0400

    Created Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AngryButton />
+      <CounterButton />
+      <LightSwitchButton />
+      <TextRepeaterButton />
     </div>
   );
 }
-
 export default App;
